41083c78b5a12e4c46ddc74073d866c1a7c97dbb
plowsters <142384070+plowsters@users.noreply.github.com>
2024-01-26 02:04:26 +0000

    Added buttons, fleshed out the invest() and withdrawal() functions some more

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
+import logging
 import tkinter as tk
 from tkinter import *
 
-def invest():
-  thirty = 30
 
 
-def insidertrade():
-  forty = 40
+#invest in a stock
+def invest(vault, stockPrice, numShares):
+  numShares += 1
+  print(numShares)
+  tempA = eval(vault.replace('$', '') + " - " + str(stockPrice.replace('$', '')))
+  vault = tempA
+  print(vault)
+
+#Take your money out of the stock market
+def withdraw(vault, stockPrice, numShares):
+  numShares -= 1
+  tempA = eval(vault.replace('$', '') + " + " + str(stockPrice.replace('$', '')))
+  vault = tempA
+
+def insidertrade(numClicks):
+  numClicks += 1
+  logging.info(numClicks)
 
 
 def main():
 
   #Money storage
   vault = "${:.2f}".format(100.0)
-  print(vault)
 
+  numClicks = 0
+  
   #Dictionary containing the names and initial values of stocks
   Stocks = {
     "GME": "${:.2f}".format(25.0),
@@ -24,19 +39,53 @@ def main():
     "MSFT": "${:.2f}".format(50.0),
   }
 
-  print(Stocks.get("GME"))
-
+  #Dictionary containing the names of stocks and number of shares owned
+  Shares = {
+    "GME": 0,
+    "AAPL": 0,
+    "AMZN": 0,
+    "TSLA": 0,
+    "MSFT": 0,
+  }
   
   #Set up Game Window
   window = tk.Tk()
   window.title("Your Retirement Fund")
   w = Canvas(window, width=400, height=300)
   w.pack()
-  frame = Frame(window)
-  frame.pack( side = LEFT )
+  #Buy and sell GME stocks
+  frame1 = Frame(window)
+  frame1.pack( side = TOP )
+  investGME = tk.Button(frame1, text = 'Buy GME', fg = 'red', command = lambda: invest(vault, Stocks["GME"], Shares["GME"]))
+  investGME.pack( side = TOP)
+  divestGME = tk.Button(frame1, text = 'Sell GME', fg = 'blue', command = lambda: withdraw(vault, Stocks["GME"], Shares["GME"]))
+  divestGME.pack( side = TOP)
+  #Buy and sell AAPL and AMZN stocks
+  frame2 = Frame(window)
+  frame2.pack( side = LEFT )
+  investAAPL = tk.Button(frame2, text = 'Buy AAPL', fg = 'red', command = lambda: invest(vault, Stocks["AAPL"], Shares["AAPL"]))
+  investAAPL.pack( side = TOP )
+  divestAAPL = tk.Button(frame2, text = 'Sell AAPL', fg = 'blue' , command = lambda: withdraw(vault, Stocks["AAPL"], Shares["AAPL"]))
+  divestAAPL.pack( side = TOP )
+  investAMZN = tk.Button(frame2, text = 'Buy AMZN', fg = 'red', command = lambda: invest(vault, Stocks["AMZN"], Shares["AMZN"]))
+  investAMZN.pack( side = TOP )
+  divestAMZN = tk.Button(frame2, text = 'Sell AMZN', fg = 'blue' , command = lambda: withdraw(vault, Stocks["AMZN"], Shares["AMZN"]))
+  divestAMZN.pack( side = TOP )
+  #Buy and sell TSLA and MSFT stocks
+  frame3 = Frame(window)
+  frame3.pack( side = RIGHT )
+  investTSLA = tk.Button(frame3, text = 'Buy TSLA', fg = 'red', command = lambda: invest(vault, Stocks["TSLA"], Shares["TSLA"]))
+  investTSLA.pack( side = TOP)
+  divestTSLA = tk.Button(frame3, text = 'Sell TSLA', fg = 'blue', command = lambda: withdraw(vault, Stocks["TSLA"], Shares["TSLA"]))
+  divestTSLA.pack( side = TOP)
+  investMSFT = tk.Button(frame3, text = 'Buy MSFT', fg = 'red', command = lambda: invest(vault, Stocks["MSFT"], Shares["MSFT"]))
+  investMSFT.pack( side = TOP)
+  divestMSFT = tk.Button(frame3, text = 'Sell MSFT', fg = 'blue', command = lambda: withdraw(vault, Stocks["MSFT"], Shares["MSFT"]))
+  divestMSFT.pack( side = TOP)
   #Insider Trading Button
-  itrade = tk.Button(window, text='Insider Trading', height = 5, width = 12, command = insidertrade())
+  itrade = tk.Button(window, text='Insider Trading', height = 5, width = 15, command = lambda: insidertrade(numClicks))
   itrade.pack()
+  
   window.mainloop()
   
 #Call main() function
