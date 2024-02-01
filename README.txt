188babd16549b3ad13509c04e839ffc48ca2de90
plowsters <142384070+plowsters@users.noreply.github.com>
2024-02-01 02:42:56 +0000

    Implemented Stock Price Fluctuation
    Wrote function to increase or decrease stock price generally following a sine curve with random positive and negative fluctuation. Also implemented soft upper and lower boundaries for stock prices and error checking for negative stock prices

--- README.txt
+++ README.txt
@@ -5,3 +5,17 @@ Stock Market Game
 -Every second the market fluctuates and your stocks can go up or down in value by a random amount. They also have a random chance to go up or down 10 dollars at a time and an even smaller chance to go up or down 100 dollars in a single second
 -Wealth that is not stored in stocks is stored in your bank vault
 -Prestige mechanic at 1 million dollars in the vault that adds more available stocks and stock price manipulators
+
+-WIP
+  -Display Vault Balance
+  -Display # of stocks owned and price @ purchase
+  -Decide what Insider Trading does
+  -Stock Trend Graphs
+  -Prestige Mechanic
+
+-Ideas to implement:
+  
+  -Day/Week Tracker
+  -Source of Income
+  -Recurrent Bills (goal of the game is to keep up with increasingly expensive bills)
+  
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import logging
+import math
+import random
 import threading
 import time
 import tkinter as tk
@@ -39,19 +41,70 @@ Shares = {
   "MSFT": 0,
 }
 
-##A function that updates the stock values every second
+#Calculates how the value of each stock changes over time
+def calcStockVal(stockName):
+  global trendDownwards
+  trendDownwards = False
+  global trendUpwards
+  trendUpwards = False
+  #Produces a random integer from -45 to 45
+  x = random.randint(-45, 45)
+  #Sets the multiplier to sin((x*pi)/180) + a random
+  #float between -1 and 0.11, all rounded to 2 decimal places
+  multiplier = round(math.sin(x*math.pi/180) + random.uniform(-1, 0.11), 2)
+  tempA = float(Stocks[stockName].replace("$", ""))
+  #Prevents stock value from ever being negative or zero
+  if tempA - (tempA ** multiplier) <= 0:
+    tempA += (tempA ** multiplier)
+  #50/50 chance to add or subtract (current stock value)^(multiplier)
+  #from the stock. Also checks if a stock is increasing or decreasing
+  #too rapidly to set soft upper and lower limits to stock price
+  upperThreshold = 1500
+  lowerThreshold = 5
+  if (tempA >= upperThreshold) or (trendDownwards == 1):
+    trendDownwards = True
+    ##Gives stock an 80% chance to go down until it reaches $750
+    if random.uniform(0, 1) < 0.8:
+      tempA -= tempA**multiplier
+    else:
+      tempA += tempA**multiplier
+    if tempA <= 750:
+      trendDownwards = False
+  elif (tempA <= lowerThreshold) or (trendUpwards == 1):
+    trendUpwards = True
+    #Gives stock an 80% chance to go up until it reaches $75
+    if random.uniform(0, 1) < 0.8:
+      tempA += tempA**multiplier
+    else:
+      tempA -= tempA**multiplier
+    if tempA >= 75:
+      trendUpwards = False
+  else:
+    #Gives stock a 50% chance to go up or down if within the
+    #upper and lower thresholds
+    if random.uniform(0, 1) < 0.5:
+      tempA += tempA ** multiplier
+    else:
+      tempA -= tempA ** multiplier
+  return "${:.2f}".format(tempA)
+
+#A function that updates the stock values every second
 def updateStocks():
+  global stockLabels
+  while firstInvest == 0:
+    time.sleep(1)
+    updateStocks()
   global Stocks
-  global firstInvest
-  global secondInvest
-  global numStocks
-  while firstInvest == 1 or secondInvest == 1:
+  while True:
     for key in Stocks: 
-      Stocks[key] = eval((Stocks[key].replace('$', '') + " + " + (Stocks[key].replace('$','')))) * 0.1
-      Stocks[key] = "${:.2f}".format(Stocks[key])
-      print(Stocks[key])
-  time.sleep(1)
-  updateStocks()
+      Stocks[key] = calcStockVal(key)
+    if stockLabels:
+      stockLabels[0].config(text="GME: " + Stocks["GME"])
+      stockLabels[1].config(text="AAPL: " + Stocks["AAPL"])
+      stockLabels[2].config(text="AMZN: " + Stocks["AMZN"])
+      stockLabels[3].config(text="TSLA: " + Stocks["TSLA"])
+      stockLabels[4].config(text="MSFT: " + Stocks["MSFT"])
+    time.sleep(0.5)
 
 #Checks if the user has enough money to buy the stock
 def errorCheck(tempA):
@@ -65,9 +118,6 @@ def errorCheck(tempA):
 def invest(vaultParam, stockPrice, numShares, stockName):
   global vault
   global firstInvest
-  global secondInvest
-  if vault != "${:.2f}".format(100.0):
-    secondInvest = True
   tempA = eval(vaultParam.replace('$', '') + " - " + (stockPrice.replace('$', '')))
   cancelTransaction = errorCheck(tempA)
   if cancelTransaction == 0:
@@ -76,8 +126,6 @@ def invest(vaultParam, stockPrice, numShares, stockName):
   numShares += 1
   Shares[stockName] = numShares
   firstInvest = True
-  if secondInvest:
-    firstInvest = False
   return
   print(vault)
 
@@ -101,32 +149,27 @@ def insidertrade(clickCounter):
 
 def main():
 
+  continuous_thread = threading.Thread(target=updateStocks)
+  continuous_thread.start()
   #Set up Game Window
   window = tk.Tk()
   window.title("Your Retirement Fund")
   window.geometry("500x500")
   sFrame1 = Frame(window, bg = "red")
-  GMELabel = Label(sFrame1, text="GME: " + Stocks["GME"], bg = "red")
-  GMELabel.pack()
+  global stockLabels
+  stockLabels = [
+    Label(sFrame1, text="GME: " + Stocks["GME"], bg = "red"),
+    Label(sFrame1, text="AAPL: " + Stocks["AAPL"], bg = "red"),
+    Label(sFrame1, text="AMZN: " + Stocks["AMZN" ], bg = "red"),
+    Label(sFrame1, text="TSLA: " + Stocks["TSLA"], bg = "red"),
+    Label(sFrame1, text="MSFT: " + Stocks["MSFT"], bg = "red")
+  ]
+  for label in stockLabels:
+    label.pack()
   sFrame1.pack()
-  sFrame2 = Frame(window, bg = "red")
-  AAPLLabel = Label(sFrame2, text="AAPL: " + Stocks["AAPL"], bg = "red")
-  AAPLLabel.pack()
-  sFrame2.pack()
-  sFrame3 = Frame(window, bg = "red")
-  AMZNLabel = Label(sFrame3, text="AMZN: " + Stocks["AMZN"], bg = "red")
-  AMZNLabel.pack()
-  sFrame3.pack()
-  sFrame4 = Frame(window, bg = "red")
-  TSLALabel = Label(sFrame4, text="TSLA: " + Stocks["TSLA"], bg = "red")
-  TSLALabel.pack()
-  sFrame4.pack()
-  sFrame5 = Frame(window, bg = "red")
-  MSFTLabel = Label(sFrame5, text="MSFT: " + Stocks["MSFT"], bg = "red")
-  MSFTLabel.pack()
-  sFrame5.pack()
-  w = Canvas(window, width=300, height=300)
   
+  w = Canvas(window, width=300, height=200)
+
   w.pack(side = TOP )
   #Buy and sell GME stocks
   frame1 = Frame(window)
